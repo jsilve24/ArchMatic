0bb34607b625ff8aab14c9a64556915b4a793121
John Bateman <john.j.bateman@gmail.com>
2020-08-22 21:43:53 -0400

    Update postsoftwaresetup.sh

--- postsoftwaresetup.sh
+++ postsoftwaresetup.sh
@@ -22,3 +22,16 @@ sed -i 's/^gtk-icon-theme-name=Adwaita/gtk-icon-theme-name=Papirus-Dark/' ~/.con
 echo 'awesome.restart()' | awesome-client
 
 # Configure zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+yay -S powerline-fonts fzf --noconfirm --needed
+
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/' ~/.zshrc
+git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git ~/.solarized
+sh ~/.solarized/install.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+mv ~/.zshrc .zshrc.bak
+mv .zshrc ~/.zshrc
